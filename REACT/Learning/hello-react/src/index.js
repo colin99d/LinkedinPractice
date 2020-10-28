4277f78cf2287ba74106eec2259f84e1ff3f4575
colin99d <colin99delahunty@gmail.com>
2020-10-28 11:51:12 -0400

    Finished 1st react project

--- REACT/Learning/hello-react/src/index.js
+++ REACT/Learning/hello-react/src/index.js
@@ -52,9 +52,32 @@ function Checkbox() {
   )
 }
 
+function GitHubUser({login}) {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch('https://api.github.com/users/'+login)
+      .then( res => res.json())
+      .then(setData)
+      .catch(console.error);
+  }, []);
+  if(data) {
+    return(
+      <div>
+        <h1>{data.login}</h1>
+        <img src={data.avatar_url} width={100} />
+      </div>
+    );
+  }
+  return null;
+}
+
+function Appp () {
+  return <GitHubUser login="colin99d" />
+}
+
 
 ReactDOM.render(
-  <Checkbox />,
+  <Appp />,
   document.getElementById('root')
 );
 
