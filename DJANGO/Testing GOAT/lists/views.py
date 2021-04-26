9059946784818da9cd0c6db8203bcc782fdabb15
Colin Delahunty <colindelahunty@149-160-137-45.dhcp-bl.indiana.edu>
2021-04-26 12:58:51 -0400

    Continued testing GOAT

--- DJANGO/Testing GOAT/lists/views.py
+++ DJANGO/Testing GOAT/lists/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item
 
 # Create your views here.
+def home_page(request):
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/')  
+
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
